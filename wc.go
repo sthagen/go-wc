b0991d57e249fe9c32e511c7f783df0b65e0c048
Stefan Hagen <stefan@hagen.link>
2021-04-17 19:48:12 +0200

    Initial no brain word counter

--- wc.go
+++ wc.go
@@ -1,1 +1,27 @@
+package main
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+// Call the count function to count the number of words
+// received from the Standard Input and printing it out
+func main() {
+	fmt.Println(count(os.Stdin))
+}
+
+// Count the words received from the reader
+func count(r io.Reader) int {
+	scanner := bufio.NewScanner(r)
+	// Define the scanner split type to words (default is split by lines)
+	scanner.Split(bufio.ScanWords)
+
+	wc := 0
+	for scanner.Scan() {
+		wc++
+	}
+	return wc
+}
