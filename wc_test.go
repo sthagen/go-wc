ecc0aca3e5592f86eb301144068fe0378a4ee098
Stefan Hagen <stefan@hagen.link>
2021-04-17 19:55:19 +0200

    Added test for line counting

--- wc_test.go
+++ wc_test.go
@@ -14,3 +14,13 @@ func TestCountWords(t *testing.T) {
 		t.Errorf("Expected %d, got %d instead.\n", exp, res)
 	}
 }
+
+// TestCountLines tests the count function set to count lines
+func TestCountLines(t *testing.T) {
+	b := bytes.NewBufferString("word1 word2 word3\nline2\nline3 word1")
+	exp := 3
+	res := count(b, true)
+	if res != exp {
+		t.Errorf("Expected %d, got %d instead.\n", exp, res)
+	}
+}
