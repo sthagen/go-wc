464e765141588aa21957050a22c8d3e6e13d8674
Stefan Hagen <stefan@hagen.link>
2021-04-17 19:54:18 +0200

    Fixed test

--- wc_test.go
+++ wc_test.go
@@ -9,7 +9,7 @@ import (
 func TestCountWords(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3 word4\n")
 	exp := 4
-	res := count(b)
+	res := count(b, false)
 	if res != exp {
 		t.Errorf("Expected %d, got %d instead.\n", exp, res)
 	}
