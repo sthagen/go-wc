bc9ed8be53559ce1169b84733727a75236bf6393
Stefan Hagen <stefan@hagen.link>
2021-04-17 20:16:44 +0200

    Initial CLI

--- cmd/wc/main.go
+++ cmd/wc/main.go
@@ -1,1 +1,17 @@
+package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	wc "github.com/sthagen/go-wc"
+)
+
+// Call the count function to count the number of words
+// received from the Standard Input and printing it out
+func main() {
+	lines := flag.Bool("l", false, "Count lines not words")
+	flag.Parse()
+	fmt.Println(wc.Count(os.Stdin, *lines))
+}
